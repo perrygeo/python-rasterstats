bffa936d052bd64f053211d41e1313c14ca2c25b
Matthew Perry <mperry@ecotrust.org>
2013-09-21 13:58:55 -0700

    include stuff for building html notebooks

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -10,10 +10,10 @@ sudo apt-get install -y libgdal-dev gdal-bin \
                    python-gdal python-pip python-numpy \
                    libspatialindex-dev libspatialindex1 \
                    build-essential git atop python-dev \
-                   libfreetype6 libfreetype6-dev libpng12-dev screen
+                   libfreetype6 libfreetype6-dev libpng12-dev screen pandoc
 
 
 cd /usr/local/src/python-raster-stats
 sudo pip install --upgrade setuptools
-sudo pip install --upgrade shapely pytest coverage tornado jinja2 pyzmq ipython matplotlib
+sudo pip install --upgrade shapely pytest coverage tornado jinja2 pyzmq ipython matplotlib pygments
 python setup.py develop
