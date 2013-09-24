37977b14f02ad130a3f2eda0aa885676b389c055
Matthew Perry <mperry@ecotrust.org>
2013-09-24 10:43:32 -0700

    version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(TestCommand):
 
 setup(
     name="rasterstats",
-    version="0.3",
+    version="0.3.1",
     author="Matthew Perry",
     author_email="perrygeo@gmail.com",
     description=("Summarize geospatial raster datasets based on vector geometries"),
