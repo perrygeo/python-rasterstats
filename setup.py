0e54b24ce55c1565ac1538b18ec6fd8af07604d5
Matthew Perry <perrygeo@gmail.com>
2014-05-28 08:05:17 -0700

    version bump

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class PyTest(TestCommand):
 
 setup(
     name="rasterstats",
-    version="0.3.4",
+    version="0.3.5",
     author="Matthew Perry",
     author_email="perrygeo@gmail.com",
     description=("Summarize geospatial raster datasets based on vector geometries"),
