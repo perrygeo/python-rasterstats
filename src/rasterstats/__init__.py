5b5ba05fe3fa4754d3fca9c510458554e97a8733
Matthew Perry <perrygeo@gmail.com>
2015-07-31 14:20:16 -0400

    add clinamespace

--- src/rasterstats/__init__.py
+++ src/rasterstats/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from .main import raster_stats, zonal_stats
+from rasterstats import cli
 
-__all__ = ['raster_stats', 'zonal_stats']
+__all__ = ['raster_stats', 'zonal_stats', 'cli']
