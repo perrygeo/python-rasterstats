cb5d9dafc675ad581e5bc34cef229a62884ddd39
Matthew Perry <perrygeo@gmail.com>
2015-08-25 09:47:29 -0400

    2.7 error with byte strings

--- src/rasterstats/io.py
+++ src/rasterstats/io.py
@@ -67,7 +67,7 @@ def get_features(vectors, layer_num=0):
 
             features_iter = fiona_generator(vectors)
             strategy = 'fiona'
-        except (AssertionError, IOError, OSError):
+        except (AssertionError, TypeError, IOError, OSError):
             # ... or a single string to be parsed as wkt/wkb/json
             feat = parse_geo(vectors)
             features_iter = [feat]
