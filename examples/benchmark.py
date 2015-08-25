5db73bf752070b9773a53af5444567bc84cdff80
Matthew Perry <perrygeo@gmail.com>
2015-08-25 12:03:40 -0400

    benchmark, towards #47

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -1,14 +1,32 @@
+from __future__ import print_function
+"""
+First, download the data and place in `benchmark_data`
+
+1. Download countries from
+    wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip
+    unzip
+
+2. Download the SRTM data from
+    https://hc.app.box.com/shared/1yidaheouv
+    Password is `ThanksCSI!`
+    select `SRTM_1km_TIF.rar`
+    $ unrar e SRTM_1km_TIF.rar
+
+Runtime history:
+   1bc8711 130.93s MacBook Pro (Retina, 15-inch, Mid 2014) 2.2GHz i7, 16GB RAM
+"""
 from rasterstats import raster_stats
 import time
 
 class Timer():
     def __enter__(self):
         self.start = time.time()
+
     def __exit__(self, *args):
-        print "Time:", time.time() - self.start
+        print("Time:", time.time() - self.start)
 
-states = '/data/workspace/rasterstats_blog/boundaries_contus.shp'
-precip = '/data/workspace/rasterstats_blog/NA_Annual_Precipitation_GRID/NA_Annual_Precipitation/data/na_anprecip/hdr.adf'
+countries = "./benchmark_data/ne_50m_admin_0_countries.shp"
+elevation = "./benchmark_data/SRTM_1km.tif"
+
 with Timer():
-    stats = raster_stats(states, precip, stats="*")
-
+    stats = raster_stats(countries, elevation, stats="*")
