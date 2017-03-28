ac66944a9de41f3d15521f3f4a29f38a4cb21d28
userw <sgoodman@aiddata.org>
2017-03-28 14:35:07 -0400

    Clean up test order

--- tests/test_zonal.py
+++ tests/test_zonal.py
@@ -457,10 +457,67 @@ def test_geojson_out():
         assert 'id' in feature['properties']  # from orig
         assert 'count' in feature['properties']  # from zonal stats
 
+# do not think this is actually testing the line i wanted it to
+# since the read_features func for this data type is generating
+# the properties field
+def test_geojson_out_with_no_properties():
+    polygon = Polygon([[0, 0], [0, 0,5], [1, 1.5], [1.5, 2], [2, 2], [2, 0]])
+    arr = np.array([
+        [100, 1],
+        [100, 1]
+    ])
+    affine = Affine(1, 0, 0,
+                    0, -1, 2)
+
+    stats = zonal_stats(polygon, arr, affine=affine, geojson_out=True)
+    assert 'properties' in stats[0]
+    for key in ['count', 'min', 'max', 'mean']:
+        assert key in stats[0]['properties']
+
+    assert stats[0]['properties']['mean'] == 34
+
+
+# remove when copy_properties alias is removed
+def test_copy_properties_warn():
+    polygons = os.path.join(DATA, 'polygons.shp')
+    # run once to trigger any other unrelated deprecation warnings
+    # so the test does not catch them instead
+    stats_a = zonal_stats(polygons, raster)
+    with pytest.deprecated_call():
+        stats_b = zonal_stats(polygons, raster, copy_properties=True)
+    assert stats_a == stats_b
+
+
+def test_nan_counts():
+    transform = Affine(1, 0, 1, 0, -1, 3)
+
+    data = np.array([
+        [np.nan, np.nan, np.nan],
+        [0, 0, 0],
+        [1, 4, 5]
+    ])
+
+    # geom extends an additional row to left
+    geom = 'POLYGON ((1 0, 4 0, 4 3, 1 3, 1 0))'
+
+    # nan stat is requested
+    stats = zonal_stats(geom, data, affine=transform, nodata=0.0, stats="*")
+
+    for res in stats:
+        assert res['count'] == 3  # 3 pixels of valid data
+        assert res['nodata'] == 3  # 3 pixels of nodata
+        assert res['nan'] == 3  # 3 pixels of nans
+
+    # nan are ignored if nan stat is not requested
+    stats = zonal_stats(geom, data, affine=transform, nodata=0.0, stats="count nodata")
+
+    for res in stats:
+        assert res['count'] == 3  # 3 pixels of valid data
+        assert res['nodata'] == 3  # 3 pixels of nodata
+        assert 'nan' not in res
+
 
 def test_array_overlap_counts():
-    from affine import Affine
-
     nodata = -9999
     no_overlap = -8888
 
@@ -495,67 +552,6 @@ def test_raster_overlap_counts():
         assert res['count'] == 9  # Two pixels of valid data
         assert res['nodata'] == 3  # Two pixels of nodata
         assert res['no_overlap'] == 3  # Three pixels of no overlap
-
-
-# do not think this is actually testing the line i wanted it to
-# since the read_features func for this data type is generating
-# the properties field
-def test_geojson_out_with_no_properties():
-    polygon = Polygon([[0, 0], [0, 0,5], [1, 1.5], [1.5, 2], [2, 2], [2, 0]])
-    arr = np.array([
-        [100, 1],
-        [100, 1]
-    ])
-    affine = Affine(1, 0, 0,
-                    0, -1, 2)
-
-    stats = zonal_stats(polygon, arr, affine=affine, geojson_out=True)
-    assert 'properties' in stats[0]
-    for key in ['count', 'min', 'max', 'mean']:
-        assert key in stats[0]['properties']
-
-    assert stats[0]['properties']['mean'] == 34
-
-
-# remove when copy_properties alias is removed
-def test_copy_properties_warn():
-    polygons = os.path.join(DATA, 'polygons.shp')
-    # run once to trigger any other unrelated deprecation warnings
-    # so the test does not catch them instead
-    stats_a = zonal_stats(polygons, raster)
-    with pytest.deprecated_call():
-        stats_b = zonal_stats(polygons, raster, copy_properties=True)
-    assert stats_a == stats_b
-
-
-def test_nan_counts():
-    from affine import Affine
-    transform = Affine(1, 0, 1, 0, -1, 3)
-
-    data = np.array([
-        [np.nan, np.nan, np.nan],
-        [0, 0, 0],
-        [1, 4, 5]
-    ])
-
-    # geom extends an additional row to left
-    geom = 'POLYGON ((1 0, 4 0, 4 3, 1 3, 1 0))'
-
-    # nan stat is requested
-    stats = zonal_stats(geom, data, affine=transform, nodata=0.0, stats="*")
-
-    for res in stats:
-        assert res['count'] == 3  # 3 pixels of valid data
-        assert res['nodata'] == 3  # 3 pixels of nodata
-        assert res['nan'] == 3  # 3 pixels of nans
-
-    # nan are ignored if nan stat is not requested
-    stats = zonal_stats(geom, data, affine=transform, nodata=0.0, stats="count nodata")
-
-    for res in stats:
-        assert res['count'] == 3  # 3 pixels of valid data
-        assert res['nodata'] == 3  # 3 pixels of nodata
-        assert 'nan' not in res
 
 
 # Optional tests
