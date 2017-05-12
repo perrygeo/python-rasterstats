6ce560df6911ea70e108735ececfecbe5243c27d
Olly Clements <olcl@pml.ac.uk>
2017-05-12 10:15:35 +0100

    forcing build test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,7 +18,6 @@ def test_cli_feature():
                                         '--stats', 'mean',
                                         '--prefix', 'test_'])
     assert result.exit_code == 0
-    print result.output
     outdata = json.loads(result.output)
     assert len(outdata['features']) == 1
     feature = outdata['features'][0]
